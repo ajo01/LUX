0e63fc75772999d3c345249c428f3d8aa2732a5d
ajo01 <amyjojjm2001@gmail.com>
2021-06-29 13:48:56 -0700

    forminput component

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
 
 const AddressForm = () => {
-  return <div>Address form</div>;
+  const methods = useForm();
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit>
+          <Grid container spacing={3}></Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
