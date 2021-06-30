ae21638f35442e3b5a613e17ae0cdcd32b652d1d
ajo01 <amyjojjm2001@gmail.com>
2021-06-30 16:38:59 -0700

    modalview shows selectedimg

--- src/components/ModalView/ModalView.jsx
+++ src/components/ModalView/ModalView.jsx
@@ -3,11 +3,18 @@ import styles from "./ModalView.module.css";
 
 const ModalView = ({ selectedImg, setSelectedImg }) => {
   const handleClick = (e) => {
+    console.log("clicked");
+    console.log(e.target.classList);
     if (e.target.classList.contains("backdrop")) {
       setSelectedImg(null);
     }
   };
-  return <div className={styles.backdrop}></div>;
+
+  return (
+    <div className={styles.backdrop} onClick={handleClick}>
+      <img src={selectedImg} alt="enlarged-pic"></img>
+    </div>
+  );
 };
 
 export default ModalView;
