77b3abeb921aa2cc3029f584b1ebf1cbd8a516fc
ajo01 <amyjojjm2001@gmail.com>
2021-06-29 23:16:34 -0700

    paymentform shows

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -30,7 +30,6 @@ const Checkout = ({ cart }) => {
         const token = await commerce.checkout.generateToken(cart.id, {
           type: "cart",
         });
-        console.log(token);
         setCheckoutToken(token);
       } catch (e) {}
     };
@@ -52,7 +51,7 @@ const Checkout = ({ cart }) => {
     activeStep === 0 ? (
       <AddressForm checkoutToken={checkoutToken} next={next} />
     ) : (
-      <PaymentForm shippingData={shippingData} />
+      <PaymentForm checkoutToken={checkoutToken} shippingData={shippingData} />
     );
 
   return (
--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Typography, Button, Divider } from "@material-ui/core";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Review from "./Review";
 
-const PaymentForm = () => {
-  return <div>Payment Form</div>;
+const PaymentForm = ({ checkoutToken, shippingData }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
 };
 
 export default PaymentForm;
